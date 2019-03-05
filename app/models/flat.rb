9e046ee596e4d5062a70c117cf10b28810097e6b
Nicolas Gil Michel <ngilmichel@gmail.com>
2019-03-05 14:33:08 +0100

    simple_form + uploader

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,8 +1,10 @@
 class Flat < ApplicationRecord
+  mount_uploader :picture, PictureUploader
+
   validates :title, presence: true
   validates :address, presence: true
   validates :city, presence: true
   validates :description, presence: true
   validates :price_night, presence: true
-  validates :picture, presence: true
+  # validates :picture, presence: true
 end
