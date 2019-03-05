76091253313469519ea605d435ec7ff15441f097
francesc ordeig <f.ordeig@gmail.com>
2019-03-05 11:07:50 +0100

    add model flats and create database/migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_175123) do
+ActiveRecord::Schema.define(version: 2019_03_05_100543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.integer "price_night"
+    t.string "description"
+    t.integer "id_user"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
