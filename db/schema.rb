056e23da48967250da2d67da2406875c6b64c0dd
francesc ordeig <f.ordeig@gmail.com>
2019-03-06 18:21:03 +0100

    add columns capacity and type to flats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_164154) do
+ActiveRecord::Schema.define(version: 2019_03_06_171420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_03_06_164154) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.integer "capacity"
+    t.string "type"
   end
 
   create_table "users", force: :cascade do |t|
