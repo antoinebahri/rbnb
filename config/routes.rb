3913f4f2dcc57291b542a9ac8d8c638818ad8e53
Doncho Penev <b.d.penev@gmail.com>
2019-03-07 02:43:50 +0100

    seed file updated, resources form in routes, index of bookings started, need to do booking card first

--- config/routes.rb
+++ config/routes.rb
@@ -11,3 +11,29 @@ Rails.application.routes.draw do
   get '/contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# Prefix Verb   URI Pattern                                           Controller#Action
+#             flats GET    /flats(.:format)                                      flats#index
+#                   POST   /flats(.:format)                                      flats#create
+#          new_flat GET    /flats/new(.:format)                                  flats#new
+#         edit_flat GET    /flats/:id/edit(.:format)                             flats#edit
+#              flat GET    /flats/:id(.:format)                                  flats#show
+#                   PATCH  /flats/:id(.:format)                                  flats#update
+#                   PUT    /flats/:id(.:format)                                  flats#update
+#                   DELETE /flats/:id(.:format)                                  flats#destroy
+#     flat_bookings GET    /flats/:flat_id/bookings(.:format)                    bookings#index
+#                   POST   /flats/:flat_id/bookings(.:format)                    bookings#create
+#  new_flat_booking GET    /flats/:flat_id/bookings/new(.:format)                bookings#new
+# edit_flat_booking GET    /flats/:flat_id/bookings/:id/edit(.:format)           bookings#edit
+#      flat_booking GET    /flats/:flat_id/bookings/:id(.:format)                bookings#show
+#                   PATCH  /flats/:flat_id/bookings/:id(.:format)                bookings#update
+#                   PUT    /flats/:flat_id/bookings/:id(.:format)                bookings#update
+#                   DELETE /flats/:flat_id/bookings/:id(.:format)                bookings#destroy
+#                   GET    /flats(.:format)                                      flats#index
+#                   POST   /flats(.:format)                                      flats#create
+#                   GET    /flats/new(.:format)                                  flats#new
+#                   GET    /flats/:id/edit(.:format)                             flats#edit
+#                   GET    /flats/:id(.:format)                                  flats#show
+#                   PATCH  /flats/:id(.:format)                                  flats#update
+#                   PUT    /flats/:id(.:format)                                  flats#update
+#                   DELETE /flats/:id(.:format)                                  flats#delete
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,30 @@ require 'faker'
 
 puts "clearing up DB"
 Flat.destroy_all
+Booking.destroy_all
 
+# creating an empty array for the indices of the flats in db and setting up counter
+flats_index_array = []
+counter=0
+
+puts 'creating 20 flats'
 20.times do
   url = 'https://source.unsplash.com/collection/1134892'
   flat = Flat.create(title: Faker::TvShows::Simpsons.location, address: Faker::Address.full_address, city: Faker::Address.city, price_night: rand(100..5000), description: Faker::Restaurant.description, id_user: rand(1..10), remote_picture_url: url)
   p flat.title + " created! :)"
 end
 
-puts "Done!"
+puts 'seeding the empty array with the indices'
+Flat.all.each do |flat|
+  flats_index_array<< flat.id
+end
 
+20.times do
+  booking = Booking.new(start_date: Date.strptime('03-06-2019', '%d-%m-%Y'), end_date: Date.strptime('03-07-2019', '%d-%m-%Y'))
+  booking.id_flat = flats_index_array[counter]
+  counter += 1
+  booking.save
+end
+
+puts "Done!"
 
