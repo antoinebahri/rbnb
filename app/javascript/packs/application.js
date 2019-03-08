8a698995d350433b78b83eb7e0a7c590b661835c
Doncho Penev <b.d.penev@gmail.com>
2019-03-08 15:52:40 +0100

    dash_book_status

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,2 @@
 import "bootstrap";
-// import "calculate-price";
+import "calculate-price";
--- app/javascript/packs/calculate-price.js
+++ app/javascript/packs/calculate-price.js
@@ -1,8 +1,8 @@
-// const startDateInput = document.querySelector("#start_date");
-// const endDateInput = document.querySelector("#end_date");
+const startDateInput = document.querySelector("#start_date");
+const endDateInput = document.querySelector("#end_date");
 
-// const TotalPrice = document.getElementById("total_price");
+const TotalPrice = document.getElementById("total_price");
 
-// TotalPrice.innerHTML = "Hello"
+TotalPrice.innerHTML = "Hello"
 
-// export "calculate-price"
+export "calculate-price"
